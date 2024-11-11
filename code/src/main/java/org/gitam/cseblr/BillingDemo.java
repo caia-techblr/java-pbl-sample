6c5d9c9e8e5f45129db317a8587f05b112927833
Rajesh Sola <rsola32@gitam.edu>
2024-11-11 23:01:58 +0530

    fixed class name

--- code/src/main/java/org/gitam/cseblr/BillingDemo.java
+++ code/src/main/java/org/gitam/cseblr/BillingDemo.java
@@ -1,5 +1,5 @@
 package org.gitam.cseblr;
-import org.gitam.cseblr.lib.CustomerDb;
+import org.gitam.cseblr.lib.CustomerList;
 
 public class BillingDemo {
     public static void main(String args[])
